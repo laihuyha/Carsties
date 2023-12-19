b9b322a7bc2b9966934ef6bb74597320436df6ed
Lại Huy Hà <laihuyha@gmail.com>
2023-12-19 17:05:12 +0700

    Create Auction Entity and it own relationship entity

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AuctionService.Entities
+{
+    public class Auction : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public int ReservePrice { get; set; }
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public int? SoldAmount { get; set; }
+        public int? CurrentHighBid { get; set; }
+        public DateTime AuctionEnd { get; set; }
+        public Status Status { get; set; }
+        public Item Item { get; set; }
+    }
+}
---
+++ src/AuctionService/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AuctionService.Entities
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+    }
+}
