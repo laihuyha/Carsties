0ac3773cd88079d9864ee09ec59194f08e2a8082
Lại Huy Hà <laihuyha@gmail.com>
2023-12-21 11:27:02 +0700

    Add Create, Update, Delete for auction. Fix gitingore file

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AuctionService.Data;
 using AuctionService.DTO;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -36,6 +38,69 @@ namespace AuctionService.Controllers
                             .Include(e => e.Item).AsNoTracking().AsSplitQuery()
                             .SingleOrDefaultAsync(e => e.Id.ToString() == Id);
             return auction == null ? (ActionResult<AuctionDTO>)NotFound() : (ActionResult<AuctionDTO>)mapper.Map<AuctionDTO>(auction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDTO>> CreateAuction(CreateAuctionDTO auctiondto)
+        {
+            try
+            {
+                var auction = mapper.Map<Auction>(auctiondto);
+                auction.Seller = "";
+                context.Auctions.Add(auction);
+                await context.SaveChangesAsync();
+                return CreatedAtAction(nameof(Get), new { id = auction.Id }, mapper.Map<AuctionDTO>(auction));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AuctionDTO>> UpdateAuction(Guid Id, UpdateAuctionDTO auctiondto)
+        {
+            try
+            {
+                var auction = await context.Auctions.Include(e => e.Item).SingleOrDefaultAsync(e => e.Id == Id);
+                if (auction == null) return NotFound();
+
+                auction.Item.Make = auctiondto.Make;
+                auction.Item.Model = auctiondto.Model;
+                auction.Item.Year = auctiondto.Year;
+                auction.Item.Color = auctiondto.Color;
+                auction.Item.Mileage = auctiondto.Mileage;
+
+                context.Auctions.Update(auction);
+                var result = await context.SaveChangesAsync();
+
+                return result > 0 ? Ok() : BadRequest("Error while updating");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid Id)
+        {
+            try
+            {
+                var auction = await context.Auctions.FindAsync(Id);
+                if (auction == null) return NotFound();
+
+                if (auction.Seller == "") // Check if current user is the seller of the auction
+                {
+                    context.Auctions.Remove(auction);
+                }
+                var result = await context.SaveChangesAsync();
+                return result > 0 ? Ok() : BadRequest("Error while deleting");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
