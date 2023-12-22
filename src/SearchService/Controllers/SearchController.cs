1220dc8c5e1c00fc7294da2afb2a7b24f693af8f
Huy Hà <laihuyha@gmail.com>
2023-12-22 22:10:20 +0700

    okay fixed

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -14,16 +14,15 @@ namespace SearchService.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Item>>> SearchItem([FromBody] SearchParams searchParams)
         {
-            // var (term, page, size, seller, winner, order, filter) = searchParams;
-            // var query = DB.PagedSearch<Item>().Sort(x => x.Ascending(a => a.Make));
+            var (searchTerm, pageNum, pageSize) = searchParams.GetSearchParams();
+            var query = DB.PagedSearch<Item>().Sort(x => x.Ascending(a => a.Make));
 
-            // if (!string.IsNullOrEmpty(searchTerm))
-            // {
-            //     _ = query.Match(Search.Full, searchTerm).SortByTextScore();
-            // }
-            // var (Results, TotalCount, PageCount) = await query.PageNumber(pageNum).PageSize(pageSize).ExecuteAsync();
-            // return Ok(new { results = Results, pageCount = PageCount, totalCount = TotalCount });
-            return Ok();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                _ = query.Match(Search.Full, searchTerm).SortByTextScore();
+            }
+            var (Results, TotalCount, PageCount) = await query.PageNumber(pageNum).PageSize(pageSize).ExecuteAsync();
+            return Ok(new { results = Results, pageCount = PageCount, totalCount = TotalCount });
         }
     }
 }
