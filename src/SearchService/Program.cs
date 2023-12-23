fcc313e11e9e9f283b98d7017cb5fccd76ebdc13
Huy Hà <laihuyha@gmail.com>
2023-12-23 12:28:34 +0700

    Making Http communication resilient

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Extensions;
 using SearchService.Services;
 
@@ -8,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
@@ -22,8 +27,18 @@ app.MapControllers();
 
 #region Add Extensions
 
-await app.UseAppBuilderExtension(builder.Configuration);
+// Config the lifetime of the application
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    await app.UseAppBuilderExtension(builder.Configuration);
+});
 
 #endregion
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+}
