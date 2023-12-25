0c1d32584d686626db2e62f395b10ed2c70e1f5c
Huy Hà <laihuyha@gmail.com>
2023-12-25 20:54:39 +0700

    Explicit config endpoint  for UseMessageRetry specified with other Consumer class

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -20,15 +20,23 @@ builder.Services.AddMassTransit(x =>
 {
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    x.AddConfigureEndpointsCallback((name, cfg) => { cfg.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(10))); });
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
-            e.UseMessageRetry(r =>
-            {
-                r.Interval(5, TimeSpan.FromSeconds(10));
-            });
+            e.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(10)));
             e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+        cfg.ReceiveEndpoint("search-auction-updated", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(10)));
+            e.ConfigureConsumer<AuctionUpdatedConsumer>(context);
+        });
+        cfg.ReceiveEndpoint("search-auction-deleted", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(10)));
+            e.ConfigureConsumer<AuctionDeletedConsumer>(context);
         });
         cfg.ConfigureEndpoints(context);
     });
