4ff970a7cabdfbef46c8643fb55b0746dddcd9e2
Huy Hà <Huy H>
2023-12-30 04:17:17 +0700

    aupdate

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -30,7 +30,17 @@ builder.Services.AddMassTransit(x =>
         _ = o.UsePostgres();
         o.UseBusOutbox();
     });
-    x.UsingRabbitMq((context, cfg) => { cfg.ConfigureEndpoints(context); });
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+
+        // Config for publish app can use
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+    });
 });
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
