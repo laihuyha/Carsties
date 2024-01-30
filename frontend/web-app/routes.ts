2c65dc8168465aeec15ec038d3a4263735734a2f
Huy Hà <laihuyha@gmail.com>
2024-01-31 01:21:30 +0700

    defined route and modify middleware behavior base on each kind of route

--- frontend/web-app/routes.ts
+++ frontend/web-app/routes.ts
@@ -9,13 +9,13 @@ export const publicRoutes: string[] = ["/"];
  * These routes will redirect to the login page first if not authenticated
  * @type {string[]}
  */
-export const authRoutes: string[] = [];
+export const authRoutes: string[] = ["/session"];
 
 /**
  * The prefix of the API routes
  * Routes will be prefixed with this for authentication purposes
  * @type {string}
  */
-export const apiRoutePrefix = "/api/auth";
+export const apiAuthRoutePrefix: string = "/api/auth";
 
 export const DEFAUTLT_LOGGED_IN_REDIRECT = "/";
