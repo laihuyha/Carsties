a4efb869eb6382bdc7a7e630689a0f849333bc8e
Huy Hà <laihuyha@gmail.com>
2024-02-16 21:02:07 +0700

    add setParams to change state  for filter by seller and by winner

--- frontend/web-app/app/_nav/dropdown-user-action.tsx
+++ frontend/web-app/app/_nav/dropdown-user-action.tsx
@@ -1,78 +1,66 @@
 "use client";
 
-import { Dropdown, DropdownItem, DropdownItemProps } from "flowbite-react";
+import { useParamsStore } from "@/hooks/useParamStore";
+import { Dropdown, DropdownItem } from "flowbite-react";
 
 import { User } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai";
 import { HiCog, HiUser } from "react-icons/hi2";
-import { UrlObject } from "url";
 
 type Props = {
   user: Partial<User>;
 };
 
-type DropdownItemPropsExtends = DropdownItemProps & {
-  isLink?: boolean;
-};
-
-type Url = string | UrlObject;
-
-const dropdownAction: DropdownItemPropsExtends[] = [
-  {
-    icon: HiUser,
-    href: "/",
-    isLink: true,
-    children: "My Auctions",
-  },
-  {
-    icon: AiFillTrophy,
-    href: "/",
-    isLink: true,
-    children: "Auctions won",
-  },
-  {
-    icon: AiFillCar,
-    href: "/auctions/create",
-    isLink: true,
-    children: "Sell my car",
-  },
-  {
-    icon: HiCog,
-    href: "/session",
-    isLink: true,
-    children: "Session (dev only)",
-  },
-  {
-    icon: AiOutlineLogout,
-    onClick: () => {
-      signOut({ callbackUrl: "/" });
-    },
-    children: "Sign Out",
-  },
-];
-
 export const DropdownUserAction = ({ user }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore((state) => state.setParams);
+
+  const setWinner = () => {
+    setParams({ winner: user.name, seller: undefined });
+    if (pathname !== "/") router.push("/");
+  };
+
+  const setSeller = () => {
+    setParams({ seller: user.name, winner: undefined });
+    if (pathname !== "/") router.push("/");
+  };
+
   return (
     <div>
-      <Dropdown
-        inline
-        label={`Welcome ${user.name}`}
-        children={dropdownAction.map((e, index) => (
-          <DropdownItem
-            {...e}
-            key={index}
-            children={
-              e.isLink ? (
-                <Link href={e.href as Url}>{e.children}</Link>
-              ) : (
-                e.children
-              )
-            }
-          />
-        ))}
-      />
+      <Dropdown inline label={`Welcome ${user.name}`}>
+        <DropdownItem
+          key={"1"}
+          onClick={setSeller}
+          icon={HiUser}
+          children="My Auctions"
+        />
+        <DropdownItem
+          key={"2"}
+          onClick={setWinner}
+          icon={AiFillTrophy}
+          children="Auctions won"
+        />
+        <DropdownItem
+          key={"3"}
+          icon={AiFillCar}
+          children={<Link href={"/auctions/create"}>Sell My Car</Link>}
+        />
+        <DropdownItem
+          key={"4"}
+          icon={HiCog}
+          children={<Link href={"/session"}>Session (dev only)</Link>}
+        />
+        <DropdownItem
+          key={"5"}
+          onClick={() => signOut({ callbackUrl: "/" })}
+          icon={AiOutlineLogout}
+          children="Sign Out"
+        />
+      </Dropdown>
     </div>
   );
 };
