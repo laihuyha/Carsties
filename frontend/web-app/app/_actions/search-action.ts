aa6ba4c88228c550727bce44db1c4c25e9f71689
Huy Hà <laihuyha@gmail.com>
2024-02-16 21:11:02 +0700

    change folder name of action to avoid to be considered to a route path

---
+++ frontend/web-app/app/_actions/search-action.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { PagedResult, SearchParams } from "@/types";
+import { Item } from "@/types/search";
+import { agent } from "../api/auth/agent";
+
+export const search = async (searchParams?: SearchParams) => {
+  let params = new URLSearchParams();
+  let res;
+  if (!searchParams) {
+    res = await agent.get<PagedResult<Item>>("/search");
+    return res;
+  }
+  Object.entries(searchParams).forEach(([key, value]) => {
+    if (value == null) return;
+    if (value.toString() !== "") params.append(key, value.toString());
+  });
+  res = await agent.get<PagedResult<Item>>(`/search?${params.toString()}`);
+  return res;
+};
