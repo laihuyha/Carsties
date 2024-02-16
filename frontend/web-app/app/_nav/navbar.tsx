8025d7ad33174a6f58f3ec4ea31641b49a40f272
Huy Hà <laihuyha@gmail.com>
2024-02-16 21:02:43 +0700

    set background color for better UX

--- frontend/web-app/app/_nav/navbar.tsx
+++ frontend/web-app/app/_nav/navbar.tsx
@@ -7,7 +7,7 @@ import { Search } from "./search";
 const Navbar = async () => {
   const user = await getCurrentUser();
   return (
-    <header className="sticky top-0 z-50 flex justify-between p-5 items-center text-gray-800 shadow-md">
+    <header className="sticky top-0 z-50 flex justify-between p-5 items-center text-gray-800 shadow-md bg-white">
       <Logo />
       <Search />
       {user ? <DropdownUserAction user={user} /> : <LoginButton />}
