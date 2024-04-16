73b63c9c1dddd76485c018dd997234a778b00e8a
Huy Hà <laihuyha@gmail.com>
2024-04-16 19:06:06 +0700

    Fix multi-threading bug

--- src/IdentityService/SeedData.cs
+++ src/IdentityService/SeedData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using IdentityModel;
 using IdentityService.Data;
 using IdentityService.Models;
@@ -14,7 +15,7 @@ namespace IdentityService;
 
 public class SeedData
 {
-    public static async void EnsureSeedData(WebApplication app)
+    public static async Task EnsureSeedData(WebApplication app)
     {
         using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
         {
