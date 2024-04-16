1553a86b2e184ada5964070a22c3eb3d0e577a5a
Huy Hà <laihuyha@gmail.com>
2024-04-16 17:54:46 +0700

    Add Grpc logic

---
+++ src/BiddingService/Services/GrpcAuctionClient.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Runtime.Internal.Util;
+using AuctionService;
+using BiddingService.Models;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BiddingService.Services
+{
+    public class GrpcAuctionClient
+    {
+        private readonly ILogger<GrpcAuctionClient> _logger;
+        private readonly IConfiguration _configuration;
+
+        public GrpcAuctionClient(ILogger<GrpcAuctionClient> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public Auction GetAuction(string id)
+        {
+            _logger.LogInformation("Calling Grpc Service");
+            var channel = GrpcChannel.ForAddress(_configuration["GrpcAuction"]);
+            var client = new GrpcAuction.GrpcAuctionClient(channel);
+            var request = new GetAuctionRequest { Id = id };
+
+            try
+            {
+                var reply = client.GetAuction(request);
+                var auction = new Auction
+                {
+                    ID = reply.Auction.Id,
+                    AuctionEnd = DateTime.Parse(reply.Auction.AuctionEnd),
+                    Seller = reply.Auction.Seller,
+                    ReservePrice = reply.Auction.ReservePrice
+                };
+                return auction;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Couldn't call Grpc Server");
+                return null;
+            }
+        }
+    }
+}
