7fa02c15bd920bae28474fd04a5753c4f1729ebc
Huy Hà <laihuyha@gmail.com>
2024-07-19 18:30:48 +0700

    create and finish component for toast

---
+++ frontend/web-app/app/_components/AuctionCreatedToast.tsx
@@ -0,0 +1,22 @@
+import { Item } from "@/types/search";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  auction: Item;
+};
+
+const AuctionCreatedToast = ({ auction }: Props) => {
+  return (
+    <Link href={`/auctions/details/${auction.id}`} className="flex flex-col items-center">
+      <div className="flex flex-col items-center gap-2">
+        <Image src={auction.imageUrl} alt="image" height={80} width={80} className="rounded-lg w-auto h-auto" />
+        <span>
+          New Auction! {auction.make} {auction.model} has been added
+        </span>
+      </div>
+    </Link>
+  );
+};
+
+export default AuctionCreatedToast;
