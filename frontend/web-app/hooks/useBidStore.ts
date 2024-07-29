fcd87e16b35cd8d60ba25413383cddb5e34cbc6e
Huy Hà <laihuyha@gmail.com>
2024-07-29 18:51:26 +0700

    add state to display if bid available

--- frontend/web-app/hooks/useBidStore.ts
+++ frontend/web-app/hooks/useBidStore.ts
@@ -4,15 +4,18 @@ import { createWithEqualityFn } from "zustand/traditional";
 
 type State = {
   bids: Bid[];
+  open: boolean;
 };
 
 type Actions = {
   setBids: (bids: Bid[]) => void;
   addBid: (bid: Bid) => void;
+  setOpen: (state: boolean) => void;
 };
 
 const initialState: State = {
   bids: [],
+  open: true,
 };
 
 export const useBidStore = createWithEqualityFn<State & Actions>()(
@@ -26,6 +29,11 @@ export const useBidStore = createWithEqualityFn<State & Actions>()(
     addBid: (bid: Bid) => {
       set((state) => ({
         bids: !state.bids.find((e) => e.id === bid.id) ? [bid, ...state.bids] : [...state.bids],
+      }));
+    },
+    setOpen: (state: boolean) => {
+      set(() => ({
+        open: state,
       }));
     },
   }),
